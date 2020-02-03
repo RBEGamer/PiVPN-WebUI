d54b7f88df19dd08e87e0c2af8ad4e67a266b52d
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2020-02-03 12:52:25 +0100

    Update startup_pivpnwebui.sh

--- startup_pivpnwebui.sh
+++ startup_pivpnwebui.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/pi
 
-
 touch /home/pi/STARTUP_SUCC.txt
 
-
 echo "--- START NODEJS WBUI ---"
-
-node /home/pi/pivpn_webui/src/server.js
-
-
-
-
-
-echo "--- STARTUP TUNNEL ---"
-
-#autossh -M 9999 -N -R *:5123:localhost:443 ubuntu@3.124.85.208
-
+node /home/pi/PiVPN-WebUI/src/server.js
 
