8fb1a9593d56d125653fd19ad000a5cd59425000
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2020-02-26 00:49:37 +0100

    Update startup_pivpnwebui.sh

--- startup_pivpnwebui.sh
+++ startup_pivpnwebui.sh
@@ -3,20 +3,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/pi
 
 
 
-# SETUP IP TALBES
-sudo iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
-
-#FINALLY ROUTE ALL TRAFFIC FROM THE OVPN POOL TO THE ETH0 
-
-echo " USING INTERFACE enxb827eb2894dd FOR OUTGOING TRAFFIC PLEASE CHANGE"
-ipaddr=$( ip -4 -o addr show enxb827eb2894dd | awk '{print $4}' | cut -d "/" -f 1)
-echo $ipaddr
-sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $ipaddr
-
-
-
-
 
 
 #python /home/pi/hd44780/show_lcd.py &
