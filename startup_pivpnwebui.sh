97b375f37c8d8baa4a2cec4fff38ea9ce8203bd9
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2020-02-26 00:50:42 +0100

    Update startup_pivpnwebui.sh

--- startup_pivpnwebui.sh
+++ startup_pivpnwebui.sh
@@ -8,6 +8,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/pi
 #python /home/pi/hd44780/show_lcd.py &
 #touch /home/pi/STARTUP_SUCC.txt
 #autossh -M 9999 -N -R *:8001:localhost:8001 ubuntu@3.124.85.208 &
+/home/pi/setup_vpn_internet.sh & 
 
 echo "--- START NODEJS WBUI ---"
 
